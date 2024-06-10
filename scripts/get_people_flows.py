56e8afb3cf171cddfb5070ff24bd5f62ffc3ffb0
Sejin Kim <hello@sejin.kim>
2024-06-10 01:05:16 -0400

    minor: Working people flows script.

--- scripts/get_people_flows.py
+++ scripts/get_people_flows.py
@@ -3,6 +3,8 @@ import pandas as pd
 from elvanto import Flows, parser
 from dotenv import load_dotenv
 from urllib.parse import urljoin
+from tqdm.auto import tqdm
+from joblib import Parallel, delayed
 
 def main():
     load_dotenv(".env")
@@ -15,23 +17,34 @@ def main():
     onboarding_flows = [flo.flows_df[flo.flows_df.name == name].index.values[0] for name in flo.flows_df.name if "TEAM ONBOARDING" in name]
     everyone = pd.DataFrame()
 
-    for flow_id in onboarding_flows:
+    def get_one_flow(flow_id):
+        single_flow = pd.DataFrame()
         steps = flo.get_flow_steps(flow_id)
         for step in steps:
             people = flo.get_step_people(step)
             pdf = pd.DataFrame.from_dict(people, orient='index')
+            
+            # Add step information
             for col in steps[step]:
                 val = steps[step][col]
                 if val is isinstance(val, str):
-                    pdf[col] = val
+                    pdf[f"step_{col}"] = val
                 else:
-                    pdf[col] = str(val)
-            everyone = pd.concat([everyone, pdf])
-        break
+                    pdf[f"step_{col}"] = str(val)
+            
+            # Add flow information
+            pdf["flow_id"] = flow_id
+            pdf["flow_name"] = flo.flows[flow_id]["name"]
+
+            single_flow = pd.concat([single_flow, pdf])
+        
+        return single_flow
+
+    all_flows = Parallel(n_jobs=-1, verbose=10)(delayed(get_one_flow)(flow_id) for flow_id in tqdm(onboarding_flows))
+    everyone = pd.concat(all_flows)
 
     everyone.to_csv("exports/Every_Person_In_People_Flows.csv")
-    # with open("db/people.json", "w") as f:
-    # json.dump(elv.people, f, indent=4)
+
 
 if __name__ == "__main__":  
     main()
